f7268706a8da6b68fe13af04aa514aff86069396
Antu <122485403+antukung@users.noreply.github.com>
2023-10-03 18:59:07 -0300

    aca esta terminaado

--- Coche.js
+++ Coche.js
@@ -1,7 +1,7 @@
 class Coche {
     #nombre = "estado default";
     #roto = 0;
-    #prendio =0;
+    #ganas= 1;
 
 
     setNombre(nombre) {
@@ -18,10 +18,15 @@ class Coche {
     }
 
     auto(conductor) {
-        if (this.#roto > 0) {
-            console.log("encendido");
+        
+    if ((conductor.getAnimo()===0)&&(this.#roto===0)) {
+        console.log("no encuentro la llave")
+        this.#ganas--
+    }
+        if (this.#roto >= 0 &&(this.#ganas===1)) {
+            console.log("encendiedo");
             conductor.movimiento(this);
-        } if (this.#roto===0) {
+        } if (this.#roto===0 &&(this.#ganas===1)) {
             console.log("no enciende");
             if (conductor.getAnimo()>0) {
                 console.log("no puedo ir me quiero matar")
